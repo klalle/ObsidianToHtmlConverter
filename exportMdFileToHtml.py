c69bd01e7fde542967916fbb8e2137186f5e3c62
Karl Hagström <karl.hagstrom@lm.se>
2021-11-22 14:19:42 +0100

    treeview of files/folders

--- exportMdFileToHtml.py
+++ exportMdFileToHtml.py
@@ -37,12 +37,10 @@ Path(os.path.dirname(destFile)).mkdir(parents=True, exist_ok=True)
 
 copyfile(mainFileToExport, destFile)
 filesAllreadyCopied = [mainFileToExport]
-if(exportToHtml):
-    with open(os.path.join(exportDir,"index.html"), 'w') as outputfile:
-        outputfile.write('<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta http-equiv="Refresh" content="0; url=\'./' + str(mainFileToExport) + '.html\'" />\n\t</head>\n</html>')
+
 
 def findRelPath(linkPath, currentFile):
-    #Hittar sökvägen relativt currentFile a la html
+    #Find filepath rel currentFile a la html
     pRoot = Path(".") #root
     pCurr = Path(currentFile)
     pLink = Path(linkPath)
@@ -66,7 +64,7 @@ def copyFileToExport(fileToFind, currentFile, traverse=False):
         destDir = os.path.join(exportDir,linkedFilePath)
         Path(os.path.dirname(destDir)).mkdir(parents=True, exist_ok=True)
         copyfile(linkedFilePath, destDir)
-        if(traverse and linkedFilePath not in filesAllreadyCopied): #förhindra cirkelreferens => oändlig loop
+        if(traverse and linkedFilePath not in filesAllreadyCopied): #prevent circle ref
             filesAllreadyCopied.append(linkedFilePath)
             readFilesRecursive(linkedFilePath)
         return findRelPath(linkedFilePath,currentFile)
@@ -74,13 +72,12 @@ def copyFileToExport(fileToFind, currentFile, traverse=False):
 def findMdFile(line, currentFile, convertHtml=True):
     pattern = re.compile(r"(?<!!)\[\[([^\]]*)\]\]")
     for (file) in re.findall(pattern, line):
-        file = file.split("#")[0] #om länk till överskrift
-        #print("fil: " + file)
+        file = file.split("#")[0] #link can also be header..
         newFile = copyFileToExport(file + '.md', currentFile, traverse=True) 
         if(convertHtml):
             if(len(newFile)>0):
                 line = line.replace('[[' + file + ']]','<a href="./' + newFile + ".html" + '">' + newFile.split("/")[-1].replace(".md","") + '</a>')
-            else: ##referar till sig själv
+            else: ##self ref
                 line = line.replace('[[' + file + ']]', '<a href="./' + file + ".md.html" + '">' + file.split("/")[-1].replace(".md","") + '</a>')
     return line
 
@@ -202,17 +199,14 @@ def readFilesRecursive(path):
                 outputfile.write(line)
             outputfile.write("</div>\n")
 
-            #Find links on this 
-            outputfile.write('<div style="width:340px; padding-top: 20px;; background-color: #DCDCDC; height: 400px; position:absolute; top:0; left:0; overflow:auto;">\n')
-            outputfile.write("<ul>")
-            for f in filesAllreadyCopied:
-                outputfile.write('<li>' + '<a href="./' + findRelPath(f, path) + ".html" + '">' + str(f).replace(".md","") + '</a>' + '</li>\n')
-            outputfile.write("</ul>")
+            outputfile.write('<div style="width:345px; padding-top: 20px;; position:absolute; top:0; left:0; overflow:auto;">\n')
+            outputfile.write('\t<iframe src="' + str(findRelPath(".",path))[:-1] + 'treeview.html" width="340px" frameBorder="0" height="900px"></iframe>\n')
             outputfile.write("</div>\n")
-            
+
             outputfile.write("</div>\n")
-            outputfile.write("</html>\n")
             outputfile.write("</body>\n")
+            outputfile.write("</html>\n")
+
     else:
         for line in data:
             findMdFile(line, currentFile=path, convertHtml=False)
@@ -222,4 +216,55 @@ def readFilesRecursive(path):
     print("Exported: " + str(path) + (" (" + str(antalAssets) + " images)" if antalAssets>0 else ''))
 
 readFilesRecursive(mainFileToExport)
+if(exportToHtml):
+    with open(os.path.join(exportDir,"index.html"), 'w') as outputfile:
+        outputfile.write('<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta http-equiv="Refresh" content="0; url=\'./' + str(mainFileToExport) + '.html\'" />\n\t</head>\n</html>')
+    
+    with open(os.path.join(exportDir,"treeview.html"), 'w') as outputfile:
+        outputfile.write("<!DOCTYPE html>\n")
+        outputfile.write("<html>\n")
+        outputfile.write("<head>\n")
+        outputfile.write('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>\n')
+        
+        outputfile.write("<head>\n")
+        outputfile.write('<base target="_parent">\n')
+        outputfile.write("<style>\n")
+        outputfile.write('ul{ padding-left: 5px; margin-left: 15px; list-style-type: "- "; }\n')
+        outputfile.write(".folderClass {list-style-type: disc;}\n")
+        outputfile.write("</style>\n")
+        outputfile.write("</head>\n")
+        
+        outputfile.write('<body style="background: #F0F0F0; ">\n')
+        filesAllreadyCopied.sort()
+        outputfile.write("<ul>")
+        for f in str(filesAllreadyCopied[0]).split("/"):
+            if('.md' in f):
+                outputfile.write('<li>' + '<a href="./' + str(filesAllreadyCopied[0]) + ".html" + '">' + str(f).replace(".md","") + '</a>' + '</li>\n')
+            else:
+                outputfile.write('<li class="folderClass">' + str(f) + "</li>")
+                outputfile.write("<ul>")
+        lastFilePath = str(filesAllreadyCopied[0]).split("/")
+        first = True
+        for currFile in filesAllreadyCopied:
+            if(not first):
+                for currFolder in str(currFile).split("/"):
+                    if(len(lastFilePath) > 0 and currFolder == lastFilePath[0]):
+                        del lastFilePath[0]
+                    else:
+                        for addedSublist in lastFilePath[:-1]: #close previous lists
+                            outputfile.write("</ul>\n")
+                        lastFilePath = ""
+                        if('.md' in currFolder):
+                            outputfile.write('<li>' + '<a href="./' + str(currFile) + ".html" + '">' + str(currFolder).replace(".md","") + '</a>' + '</li>\n')
+                        else:
+                            outputfile.write('<li class="folderClass">' + str(currFolder) + "</li>\n")
+                            outputfile.write("<ul>\n")
+                lastFilePath = str(currFile).split("/")
+            first = False
+
+        for i in str(filesAllreadyCopied[-1]).split("/"):
+            outputfile.write("</ul>")
+       
+        outputfile.write("</body>\n")
+        outputfile.write("</html>\n")
 print("Done!\n\nPath to export: " + str(exportDir) + ("/index.html" if exportToHtml else '' ))
