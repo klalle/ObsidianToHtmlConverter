dce78a035c843bf36f2c129214c1cda47d453b0f
klalle <karl.d.hagstrom@gmail.com>
2022-01-01 21:32:38 +0100

    ? ungreedy http

--- exportMdFileToHtml.py
+++ exportMdFileToHtml.py
@@ -138,7 +138,9 @@ def findExternalLinks(line):
     return line
 
 def findLinkInText(line):
-    pattern = re.compile(r"[^\">\(](https{0,1}.*)[ \n]") #un-greedy?
+    pattern = re.compile(r"[^\">\(](https{0,1}.*?)[ \n]") #?=>un-greedy
+    if "https://releases.ubuntu.com/20.04/ herunter" in line:
+        a=1
     for (link) in re.findall(pattern, line):
         line = line.replace(link,'<a href="' + link.strip() + '" target=”_blank”>' + link + "</a>")
     return line
